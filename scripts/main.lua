a181cd4a26c2068f789de85b16e5bd4a1d4b8505
James Folk <james@njligames.com>
2018-05-15 17:28:30 -0400

    first

---
+++ scripts/main.lua
@@ -0,0 +1,94 @@
+
+local Create = function()
+  print("Create")
+end
+
+local Destroy = function()
+  print("Destroy")
+end
+
+local Update = function(timeStep)
+  print("Update", timeStep)
+  njli.World.getInstance():setBackgroundColor(1.000, 1.000, 1.000)
+end
+
+local Render = function()
+  print("Render")
+end
+
+local Resize = function(width, height, orientation)
+  print("Resize", width, height, orientation)
+end
+
+local TouchesDown = function(touches)
+  print("TouchesDown")
+end
+
+local TouchesUp = function(touches)
+  print("TouchesUp")
+end
+
+local TouchesMove = function(touches)
+  print("TouchesMove")
+end
+
+local TouchesCancelled = function(touches)
+  print("TouchesCancelled")
+end
+
+local TouchDown = function(touch)
+  print("TouchDown")
+end
+
+local TouchUp = function(touch)
+  print("TouchUp")
+end
+
+local TouchMove = function(touch)
+  print("TouchMove")
+end
+
+local TouchCancelled = function(touches)
+  print("TouchCancelled")
+end
+
+local MouseDown = function(mouse)
+  print("MouseDown")
+end
+
+local MouseUp = function(mouse)
+  print("MouseUp")
+end
+
+local MouseMove = function(mouse)
+  print("MouseMove")
+end
+
+local KeyDown = function(keycodeName, withCapsLock, withControl, withShift, withAlt, withGui)
+  print("KeyDown", keycodeName, withCapsLock, withControl, withShift, withAlt, withGui)
+end
+
+local KeyUp = function(keycodeName, withCapsLock, withControl, withShift, withAlt, withGui)
+  print("KeyUp", keycodeName, withCapsLock, withControl, withShift, withAlt, withGui)
+end
+
+RegisterCreate("Create",                                         function() pcall(Create) end)
+RegisterDestroy("Destroy",                                       function() pcall(Destroy) end )
+RegisterUpdate("Update",                                         function(timeStep) pcall(Update, timeStep) end )
+RegisterRender("Render",                                         function() pcall(Render) end )
+RegisterResize("Resize",                                         function(width, height, orientation) pcall(Resize, width, height, orientation) end )
+RegisterTouchesDown("TouchesDown",                               function(touches) pcall(TouchesDown, touches) end )
+RegisterTouchesUp("TouchesUp",                                   function(touches) pcall(TouchesUp, touches) end )
+RegisterTouchesMove("TouchesMove",                               function(touches) pcall(TouchesMove, touches) end )
+RegisterTouchesCancelled("TouchesCancelled",                     function(touches) pcall(TouchesCancelled, touches) end )
+RegisterTouchDown("TouchDown",                                   function(touch) pcall(TouchDown, touch) end )
+RegisterTouchUp("TouchUp",                                       function(touch) pcall(TouchUp, touch) end )
+RegisterTouchMove("TouchMove",                                   function(touch) pcall(TouchMove, touch) end )
+RegisterTouchCancelled("TouchCancelled",                         function(touch) pcall(TouchCancelled, touch) end )
+RegisterMouseDown("MouseDown",                                   function(mouse) pcall(MouseDown, mouse) end )
+RegisterMouseUp("MouseUp",                                       function(mouse) pcall(MouseUp, mouse) end )
+RegisterMouseMove("MouseMove",                                   function(mouse) pcall(MouseMove, mouse) end )
+RegisterKeyDown("KeyDown",                                       function(keycodeName, withCapsLock, withControl, withShift, withAlt, withGui) pcall(KeyDown, keycodeName, withCapsLock, withControl, withShift, withAlt, withGui) end )
+RegisterKeyUp("KeyUp",                                         function(keycodeName, withCapsLock, withControl, withShift, withAlt, withGui) pcall(KeyUp, keycodeName, withCapsLock, withControl, withShift, withAlt, withGui) end )
+
+
